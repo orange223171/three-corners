2b8fae4f17e00b3b4961d3c8c3d1cb612c351507
orange <msyromyatnikov.06@gmail.com>
2026-04-04 16:22:04 +0300

    add Kit::add_building() and Kit::add_synergy()

--- core/src/kit.rs
+++ core/src/kit.rs
@@ -11,9 +11,9 @@ pub mod synergy;
 /// # Initialization
 pub struct Kit {
     /// A kit of building
-    pub building_kit: HashMap<String, BuildingInfo>,
+    building_kit: HashMap<String, BuildingInfo>,
     /// A kit of synergy
-    pub synergy_kit: HashMap<String, SynergyInfo>,
+    synergy_kit: HashMap<String, SynergyInfo>,
 }
 
 impl Kit {
@@ -24,4 +24,59 @@ impl Kit {
             synergy_kit: HashMap::new(),
         }
     }
+
+    /// Returns reference on the building kit
+    pub fn building_kit(&self) -> &HashMap<String, BuildingInfo> {
+        &self.building_kit
+    }
+
+    /// Returns reference on the synergy kit
+    pub fn synergy_kit(&self) -> &HashMap<String, SynergyInfo> {
+        &self.synergy_kit
+    }
+
+    /// Adds building from JSON format
+    /// # Errors
+    /// - if effect's name undefined returns [EffectNameUndefined](KitError::EffectNameUndefined)
+    /// - if JSON is uncorrected returns [JsonParsingError](KitError::JsonParsingError)
+    pub fn add_building(&mut self, json: &str) -> Result<(), KitError> {
+        match serde_json::from_str::<BuildingInfo>(json) {
+            serde_json::Result::Ok(building_info) => {
+                self.building_kit
+                    .insert(building_info.name.clone(), building_info);
+                Result::Ok(())
+            }
+            serde_json::Result::Err(_) => Result::Err(KitError::JsonParsingError),
+        }
+    }
+
+    /// Adds synergy from JSON format
+    /// # Errors
+    /// - if building's name undefiend returns [BuildingNameUndefined](KitError::BuildingNameUndefined)
+    /// - if effect's name undefined returns [EffectNameUndefined](KitError::EffectNameUndefined)
+    /// - if JSON is uncorrected returns [JsonParsingError](KitError::JsonParsingError
+    pub fn add_synergy(&mut self, json: &str) -> Result<(), KitError> {
+        match serde_json::from_str::<SynergyInfo>(json) {
+            serde_json::Result::Ok(synergy_info) => {
+                for i in 0..6 {
+                    if self
+                        .building_kit()
+                        .get(&synergy_info.buildings[i])
+                        .is_none()
+                    {
+                        return Result::Err(KitError::BuildingNameUndefined);
+                    }
+                }
+                Result::Ok(())
+            }
+            serde_json::Result::Err(_) => Result::Err(KitError::JsonParsingError),
+        }
+    }
+}
+
+/// A kit error
+pub enum KitError {
+    BuildingNameUndefined,
+    EffectNameUndefined,
+    JsonParsingError,
 }
