ee47f0b4cd334168ab35d4c2435ba5d6fe5471d1
orange <msyromyatnikov.06@gmail.com>
2026-04-07 03:29:00 +0300

    add network module, rename core to core-3c

---
+++ network/src/lib.rs
@@ -0,0 +1,8 @@
+//! Contains definitions for Three Corners's network connection
+
+/// The major version
+pub const VERSION_MAJOR: u32 = 0;
+/// The minor version
+pub const VERSION_MINOR: u32 = 1;
+/// The patch version
+pub const VERSION_PATCH: u32 = 0;
