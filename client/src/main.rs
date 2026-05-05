1383e9a2ca3a0d4ddd0a3e30c11ccd1cd32b3f7a
orange <msyromyatnikov.06@gmail.com>
2026-05-05 05:07:45 +0300

    add sfml

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,25 @@
+use sfml::{
+    graphics::{Color, RenderTarget, RenderWindow},
+    window::{ContextSettings, Event, Style, VideoMode},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = RenderWindow::new(
+        VideoMode::new(800, 600, 32),
+        "three corners",
+        Style::CLOSE,
+        &ContextSettings::default(),
+    )
+    .unwrap();
+
+    while window.is_open() {
+        while let Some(event) = window.poll_event() {
+            if event == Event::Closed {
+                window.close();
+            }
+        }
+
+        window.clear(Color::rgb(255, 127, 127));
+        window.display();
+    }
 }
