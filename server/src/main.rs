6307927d8fd00804e5a81350c46ca36a83883dd8
orange <msyromyatnikov.06@gmail.com>
2026-05-19 06:37:18 +0300

    add players_list to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -17,6 +17,7 @@ async fn main() {
     ));
 
     let mut connections_list: HashMap<SocketAddr, mpsc::Sender<Message>> = HashMap::new();
+    let mut players_list: HashMap<SocketAddr, String> = HashMap::new();
 
     let mut game = Game::new();
 
@@ -28,9 +29,17 @@ async fn main() {
                 }
                 ConnectionMessage::Disconnect(socket) => {
                     connections_list.remove(&socket);
+                    players_list.remove(&socket);
                 }
                 ConnectionMessage::Message(socket, message) => {
-                    message_handler(message, &socket, &connections_list, &mut game).await;
+                    message_handler(
+                        message,
+                        &socket,
+                        &connections_list,
+                        &mut players_list,
+                        &mut game,
+                    )
+                    .await;
                 }
             },
             None => break,
@@ -42,6 +51,7 @@ async fn message_handler(
     message: Message,
     socket: &SocketAddr,
     connections_list: &HashMap<SocketAddr, mpsc::Sender<Message>>,
+    players_list: &mut HashMap<SocketAddr, String>,
     game: &mut Game,
 ) {
     match message {
@@ -50,42 +60,64 @@ async fn message_handler(
         Message::VersionRequest => todo!(),
         Message::VersionResponce(version_responce_message) => todo!(),
         Message::LogIn(log_in_message) => {
+            players_list.insert(socket.clone(), log_in_message.player.clone());
             let message = game.add_player(log_in_message.player);
 
             for (_, sender) in connections_list {
                 sender.send(message.clone()).await.unwrap()
             }
         }
-        Message::Build(build_message) => {
-            let messages = game.build(build_message, String::from("orange"));
+        Message::Build(build_message) => match players_list.get(socket) {
+            Some(player) => {
+                let messages = game.build(build_message, player.clone());
 
-            for message in messages {
-                for (_, sender) in connections_list {
-                    sender.send(message.clone()).await.unwrap();
+                for message in messages {
+                    for (_, sender) in connections_list {
+                        sender.send(message.clone()).await.unwrap();
+                    }
                 }
             }
-        }
-        Message::Destroy(destroy_message) => {
-            let messages = game.destroy(destroy_message, String::from("orange"));
+            None => {
+                connections_list.get(&socket).expect("Not found sender").send(Message::Error(
+                        network_core::bytes_represented::error_message::ErrorMessage::OperationDenied
+                    )).await.unwrap();
+            }
+        },
+        Message::Destroy(destroy_message) => match players_list.get(socket) {
+            Some(player) => {
+                let messages = game.destroy(destroy_message, player.clone());
 
-            for message in messages {
-                for (_, sender) in connections_list {
-                    sender.send(message.clone()).await.unwrap();
+                for message in messages {
+                    for (_, sender) in connections_list {
+                        sender.send(message.clone()).await.unwrap();
+                    }
                 }
             }
-        }
-        Message::Grab(grab_message) => {
-            let messages = game.grab(grab_message, String::from("orange"));
+            None => {
+                connections_list.get(&socket).expect("Not found sender").send(Message::Error(
+                        network_core::bytes_represented::error_message::ErrorMessage::OperationDenied
+                    )).await.unwrap();
+            }
+        },
+        Message::Grab(grab_message) => match players_list.get(socket) {
+            Some(player) => {
+                let messages = game.grab(grab_message, player.clone());
 
-            for message in messages {
-                for (_, sender) in connections_list {
-                    sender.send(message.clone()).await.unwrap();
+                for message in messages {
+                    for (_, sender) in connections_list {
+                        sender.send(message.clone()).await.unwrap();
+                    }
                 }
             }
-        }
+            None => {
+                connections_list.get(&socket).expect("Not found sender").send(Message::Error(
+                        network_core::bytes_represented::error_message::ErrorMessage::OperationDenied
+                    )).await.unwrap();
+            }
+        },
         Message::SetTriangle(_) => connections_list
             .get(socket)
-            .unwrap()
+            .expect("Not found sender")
             .send(Message::Error(
                 network_core::bytes_represented::error_message::ErrorMessage::UnexpectedMessage,
             ))
@@ -93,7 +125,7 @@ async fn message_handler(
             .unwrap(),
         Message::PlayerState(_) => connections_list
             .get(socket)
-            .unwrap()
+            .expect("Not found sender")
             .send(Message::Error(
                 network_core::bytes_represented::error_message::ErrorMessage::UnexpectedMessage,
             ))
